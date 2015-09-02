01c2bd1a480ce58b2c2188050e536bd5edaa289e
James Halliday <mail@substack.net>
2015-09-01 21:27:05 -0700

    id, generate, versions and publish now working

--- bin/cmd.js
+++ bin/cmd.js
@@ -5,31 +5,70 @@ var fs = require('fs')
 
 var argv = minimist(process.argv.slice(2), {
   default: { dir: path.join(process.cwd(), 'appfeed') },
-  alias: { d: 'dir' }
+  alias: { d: 'dir', v: 'version' }
 })
 if (argv.help || argv._[0] === 'help') return usage(0)
 
 var appfeed = require('../')
-var sodium = require('sodium')
+var sodium = require('sodium').api
 var blobs = require('content-addressable-blob-store')
 var mkdirp = require('mkdirp')
+var home = require('os-homedir')()
+var defined = require('defined')
+var xtend = require('xtend')
+var tty = require('tty')
+var through = require('through2')
 
 var dbdir = path.join(argv.dir, 'db')
 var blobdir = path.join(argv.dir, 'blob')
-var keyfile = path.join(argv.dir, 'keys.json')
+var keyfile = path.resolve(process.cwd(), defined(
+  process.env.APPFEED_KEYFILE,
+  argv.keyfile,
+  path.join(home, '.config', 'appfeed.json')
+))
+
 mkdirp.sync(dbdir)
 mkdirp.sync(blobdir)
+mkdirp.sync(path.dirname(keyfile))
 
-var feed = appfeed(dbdir, sodium, {
-  store: blobs(blobdir),
-  publicKey: 
-})
-
-if (argv._[0] === 'generate') {
-  /// ...
+if (argv._[0] === 'id') {
+  fs.readFile(keyfile, function (err, src) {
+    if (err) return fail(err)
+    try { var keys = JSON.parse(src) }
+    catch (err) { return fail(err.message + ' while parsing ' + keyfile) }
+    console.log(keys.publicKey)
+  })
+} else if (argv._[0] === 'generate') {
+  var keypair = sodium.crypto_sign_keypair()
+  var value = JSON.stringify({
+    secretKey: keypair.secretKey.toString('hex'),
+    publicKey: keypair.publicKey.toString('hex')
+  }, null, 2) + '\n'
+  fs.stat(keyfile, function (err, s) {
+    if (s) return fail('refusing to overwrite existing key file: ' + keyfile)
+    fs.writeFile(keyfile, value, function (err) {
+      if (err) fail(err)
+      else if (tty.isatty()) console.error('wrote to ' + keyfile)
+    })
+  })
+} else if (argv._[0] === 'versions') {
+  var level = require('level')
+  var feed = appfeed(level(dbdir), sodium, xtend({
+    store: blobs(blobdir)
+  }, require(keyfile)))
+  feed.versions.createReadStream().pipe(
+    through.obj(function (row, enc, next) {
+      console.log(row.value.version, row.value.key)
+      next()
+    })
+  )
 } else if (argv._[0] === 'server') {
   // ...
 } else if (argv._[0] === 'publish') {
+  var level = require('level')
+  var feed = appfeed(level(dbdir), sodium, xtend({
+    store: blobs(blobdir)
+  }, require(keyfile)))
   process.stdin.pipe(feed.publish(argv, function (err, node) {
     if (err) fail(err)
     else console.log(node.key)
--- index.js
+++ index.js
@@ -3,17 +3,25 @@ var trust = require('trust-log')
 var hsodium = require('hyperlog-sodium')
 var xtend = require('xtend')
 var exchange = require('./exchange.js')
+var sub = require('subleveldown')
 
 module.exports = Appfeed
 
 function Appfeed (db, sodium, opts) {
   var self = this
-  if (!(self instanceof Appfeed)) return new Appfeed(db, opts)
+  if (!(self instanceof Appfeed)) return new Appfeed(db, sodium, opts)
   if (!opts) opts = {}
+  var keypair = {
+    secretKey: typeof opts.secretKey === 'string'
+      ? Buffer(opts.secretKey, 'hex') : opts.secretKey,
+    publicKey: typeof opts.publicKey === 'string'
+      ? Buffer(opts.publicKey, 'hex') : opts.publicKey
+  }
   self._trust = trust(sub(db, 't'), hsodium(sodium, opts, xtend(opts, {
-    publicKey: function (id, cb) { self._trust.isTrusted(id, cb) }
-  }))
-  self._versions = hyperlog(sub(db, 'l', xtend(opts, {
+    publicKey: function (id, cb) { self._trust.isTrusted(id, cb) },
+    id: opts.publicKey
+  })))
+  self.versions = hyperlog(sub(db, 'l'), xtend(opts, {
     valueEncoding: 'json',
     verify: function (node, cb) { self._trust.verify(node, cb) },
     sign: function (node, cb) {
@@ -35,20 +43,20 @@ Appfeed.prototype.replicate = function (opts, cb) {
     tstream.pipe(mux.createStream('trust')).pipe(tstream)
   }
   if (opts.versions !== false) {
-    var astream = self._versions.replicate()
+    var astream = self.versions.replicate()
     astream.pipe(mux.createStream('versions')).pipe(astream)
   }
  
   if (opts.heads) {
     if (astream) astream.once('finish', function () {
-      self._versions.heads(function (err, heads) {
+      self.versions.heads(function (err, heads) {
         if (err) return cb(err)
         else replicate(heads.map(keyof), cb)
       })
     })
   } else if (opts.full !== false) {
     if (astream) astream.once('finish', function () {
-      collect(self._versions.createReadStream(), function (err, rows) {
+      collect(self.versions.createReadStream(), function (err, rows) {
         if (err) cb(err)
         else replicate(rows.map(keyof), cb)
       })
@@ -83,13 +91,14 @@ Appfeed.prototype.revoke = function (id, cb) {
 Appfeed.prototype.publish = function (doc, cb) {
   var self = this
   if (!cb) cb = noop
-  if (!doc.version) return nextTick(cb, new Error('doc.version not provided'))
+  if (!doc.version) throw new Error('doc.version not provided')
+  if (typeof doc.version !== 'string') {
+    throw new Error('doc.version must be a string')
+  }
  
   return self._store.createWriteStream(function (err, w) {
     doc.key = w.key
-    self._versions.append(doc, function (err, node) {
-      cb(err, node.key)
-    })
+    self.versions.append(doc, cb)
   })
 }
 
